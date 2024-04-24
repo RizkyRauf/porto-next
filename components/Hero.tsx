63d312ea0cbda6062742cb5d59816a96b38beeb4
RizkyRauf <raufmoveon@gmail.com>
2024-04-24 21:37:00 +0700

    perbaikan kembali

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
                         <h1 data-aos="fade-right" className="text-[#c4cfde] mb-[1.3rem]">Welcome To My Portfolio</h1>
                         <div data-aos="fade-right" data-aos-delay="400">
                             <h1 className="xl:text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.6rem] text-white">
-                                Hi,I'm <span className="text-blue-400">Rizki Rauf</span>
+                                Hi, I&apos;m <span className="text-blue-400">Rizki Rauf</span>
                             </h1>
                         </div>
                         <p data-aos="fade-up" data-aos-delay="800" className="mt-[1.2rem] text-[15px] md:text-[17px] text-[#c4cfde]">
